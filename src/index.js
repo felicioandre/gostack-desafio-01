25d83e25904273d0c0bc28a922f2dc412534977d
André Felicio <andree.felicio@gmail.com>
2020-02-01 17:12:47 -0300

    add middlewares

--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ const server = express();
 server.use(express.json());
 
 const projects = [];
+let counter = 0;
+
+server.use((req, res, next) => {
+  console.log(`Number of Requisitions: ${++counter}`);
+  return next();
+});
+
+const verifyProject = (req, res, next) => {
+  const { id } = req.params;
+  if (projects.find(prj => prj.id === id) !== undefined) {
+    return next();
+  }
+
+  return res.status(400).json({ error: "Project not found." });
+};
 
 server.get("/projects", (req, res) => {
   return res.json(projects);
@@ -21,18 +36,17 @@ server.post("/projects", (req, res) => {
   return res.json(projects);
 });
 
-server.post("/projects/:id/tasks", (req, res) => {
+server.post("/projects/:id/tasks", verifyProject, (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
 
   project = projects.find(prj => prj.id === id);
-
   project.tasks.push(title);
 
   return res.json(projects);
 });
 
-server.put("/projects/:id", (req, res) => {
+server.put("/projects/:id", verifyProject, (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
 
@@ -42,9 +56,8 @@ server.put("/projects/:id", (req, res) => {
   return res.json(projects);
 });
 
-server.delete("/projects/:id", (req, res) => {
+server.delete("/projects/:id", verifyProject, (req, res) => {
   const { id } = req.params;
-
   const index = projects.findIndex(prj => prj.id === id);
 
   projects.splice(index, 1);
